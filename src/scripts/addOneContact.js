9245b96f5c4dba58b2c16eceec5086bbd96a962b
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2025-01-14 00:24:12 +0200

    123

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,23 @@
-export const addOneContact = async () => {};
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from '../constants/contacts.js';
 
-addOneContact();
+export const addOneContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data) || [];
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    console.log(`Контакт успешно добавлен:`, newContact);
+  } catch (error) {
+    console.error('Ошибка при добавлении контакта:', error.message);
+  }
+};
+
+
+const main = async () => {
+  await addOneContact();
+};
+
+main();
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,21 @@
-export const getAllContacts = async () => {};
+import fs from 'fs/promises'; 
+import { PATH_DB } from '../constants/contacts.js'; 
 
-console.log(await getAllContacts());
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data) || [];
+    return contacts;
+  } catch (error) {
+    console.error('Ошибка при чтении контактов:', error.message);
+    return [];
+  }
+};
+
+const main = async () => {
+  const contacts = await getAllContacts();
+  console.log('Все контакты:', contacts);
+};
+
+main();
+
